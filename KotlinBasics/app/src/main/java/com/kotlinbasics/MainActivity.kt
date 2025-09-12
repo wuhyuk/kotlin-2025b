abd0b714baa701e4fa2a71786ae4a49e0d958cc1
wuhyuk <your_email@example.com>
2025-09-12 15:49:12 +0900

    week02 : variables v0.3

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -32,15 +32,28 @@ class MainActivity : ComponentActivity() {
 }
 
 fun week02Variables(){
-    println("Week02 Variables")
+//    println("Week02 Variables")
+//
+//    val courseName = "Mobile Programming"
+//    //courseName = "IoT Programming"
+//    var week = 1
+//    week = 2
+//
+//    println("Course : $courseName")
+//    println("Week : $week")
 
-    val courseName = "Mobile Programming"
-    //courseName = "IoT Programming"
-    var week = 1
-    week = 2
+    println("== Kotlin Variables ==")
 
-    println("Course : $courseName")
-    println("Week : $week")
+//    val name: String = "Android"
+    val name = "Android"
+    var version = 8.1
+    println("Hello $name $version")
+
+    val age: Int = 23
+    val height: Double = 177.7
+    val isStudent: Boolean = false
+
+    println("Age : $age, Height : $height, Student: $isStudent")
 }
 
 @Composable
