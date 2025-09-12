3b8264a2b26c5b1310fd6134b0d83adff4ca42b0
wuhyuk <your_email@example.com>
2025-09-12 15:30:32 +0900

    week02 : README.md & variables v0.2

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -27,7 +27,20 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        week02Variables()
     }
+}
+
+fun week02Variables(){
+    println("Week02 Variables")
+
+    val courseName = "Mobile Programming"
+    //courseName = "IoT Programming"
+    var week = 1
+    week = 2
+
+    println("Course : $courseName")
+    println("Week : $week")
 }
 
 @Composable
