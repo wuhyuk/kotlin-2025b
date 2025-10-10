c134b1295ee04cfb39a1a1d60f6daab0226f85a1
deepblue <deepblue@inha.ac.kr>
2025-10-10 14:49:36 +0900

    week05 : Counter App v0.1

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -11,9 +11,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonGreeting = findViewById<Button>(R.id.buttonGreeting)
+        val buttonCounter = findViewById<Button>(R.id.buttonCount)
 
         buttonGreeting.setOnClickListener {
             startActivity(Intent(this, GreetingActivity::class.java))
+        }
+
+        buttonCounter.setOnClickListener {
+            startActivity(Intent(this, CounterActivity::class.java))
         }
     }
 }
